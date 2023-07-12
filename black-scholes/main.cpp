9e90a4b957427bc8604a9baac8cfc3d191b34469
lyndskg <lyndskg@umich.edu>
2023-07-12 11:33:17 -0500

    Initial Commit

---
+++ black-scholes/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  black-scholes
+//
+//  Created by lyndskg on 7/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
